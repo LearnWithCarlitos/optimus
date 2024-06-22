96b7ccc6dd38656e94fd88ba89ce45d9831aa7bb
DevWithCarlitos <LearnWithCarlitos@gmail.com>
2024-06-22 03:28:39 -0500

    test 1 new dep eslint-config-nestjs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,31 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  ignorePatterns: ['.eslintrc.js'],
-  rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-  },
+  extends: ['eslint-config-nestjs'],
+  // Otras configuraciones adicionales pueden ir aquí si es necesario
 };
+
+// module.exports = {
+//   parser: '@typescript-eslint/parser',
+//   parserOptions: {
+//     project: 'tsconfig.json',
+//     tsconfigRootDir: __dirname,
+//     sourceType: 'module',
+//   },
+//   plugins: ['@typescript-eslint/eslint-plugin'],
+//   extends: [
+//     'plugin:@typescript-eslint/recommended',
+//     'plugin:prettier/recommended',
+//   ],
+//   root: true,
+//   env: {
+//     node: true,
+//     jest: true,
+//   },
+//   ignorePatterns: ['.eslintrc.js'],
+//   rules: {
+//     'prettier/prettier': ['error', { endOfLine: 'lf' }],
+//     '@typescript-eslint/interface-name-prefix': 'off',
+//     '@typescript-eslint/explicit-function-return-type': 'off',
+//     '@typescript-eslint/explicit-module-boundary-types': 'off',
+//     '@typescript-eslint/no-explicit-any': 'off',
+//   },
+// };
